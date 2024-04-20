a8418c7b7d82265fc5197ee20e5368da4a323c41
Illia Lytvynov <illial@wix.com>
2024-04-20 23:47:16 +0300

    Added Iframe as sandbox

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: 'esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -26,6 +26,9 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) {
       return;
+    }
+    if (iframeRef.current) {
+      iframeRef.current.srcdoc = html;
     }
 
     const result = await ref.current.build({
@@ -41,9 +44,36 @@ const App = () => {
 
     // console.log(result);
 
-    setCode(result.outputFiles[0].text);
-  };
+    // setCode(result.outputFiles[0].text);
+    if (iframeRef.current?.contentWindow) {
+      iframeRef.current.contentWindow.postMessage(
+        result.outputFiles[0].text,
+        '*'
+      );
+    }
 
+    // eval(result.outputFiles[0].text);
+  };
+  const html = `
+    <html>
+    <head>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (e) => {
+          try {
+            eval(e.data);
+          } catch(e) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red">' + e + '</div>';
+            throw e
+          }
+        })
+      </script>
+    </body>
+    </html>
+  `;
   return (
     <div>
       <textarea
@@ -53,7 +83,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe ref={iframeRef} srcDoc={html} sandbox='allow-scripts'></iframe>
     </div>
   );
 };
