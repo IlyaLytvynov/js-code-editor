10ad0a019dd6f5d7502fcec804dc2320f82b35eb
Illia Lytvynov <illial@wix.com>
2024-05-08 23:31:03 +0300

    Split preview to single component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+
 
 const el = document.getElementById('root');
 
@@ -10,9 +14,10 @@ const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   const ref = useRef<any>();
+  const [code, setCode] = useState('')
   const [input, setInput] = useState('');
 
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+ 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
@@ -27,9 +32,7 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    if (iframeRef.current) {
-      iframeRef.current.srcdoc = html;
-    }
+    
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -42,50 +45,32 @@ const App = () => {
       },
     });
 
-    // console.log(result);
+    console.log(result);
 
-    // setCode(result.outputFiles[0].text);
-    if (iframeRef.current?.contentWindow) {
-      iframeRef.current.contentWindow.postMessage(
-        result.outputFiles[0].text,
-        '*'
-      );
-    }
+    setCode(result.outputFiles[0].text);
+   
 
     // eval(result.outputFiles[0].text);
   };
-  const html = `
-    <html>
-    <head>
-    </head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (e) => {
-          try {
-            eval(e.data);
-          } catch(e) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red">' + e + '</div>';
-            throw e
-          }
-        })
-      </script>
-    </body>
-    </html>
-  `;
+  
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe ref={iframeRef} srcDoc={html} sandbox='allow-scripts'></iframe>
+    <CodeEditor
+      initialValue="const a = 1;"
+      onChange={(value) => setInput(value)}
+    />
+    <textarea
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+    ></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
     </div>
+    <Preview code={code}/>
+  </div>
   );
 };
 
 root.render(<App />);
+
+
